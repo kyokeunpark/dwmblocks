c333ed5f237807abe8ba5f9639f645c331bd58a3
KyoKeun Park <kyokeunpark@gmail.com>
2020-10-27 16:04:46 -0400

    Added openvpn module
    Also removed the binary from repo

--- blocks.h
+++ blocks.h
@@ -5,6 +5,7 @@ static const Block blocks[] = {
 	{" ^C11^ ^d^", "get_cpu", 5, 0},
 	{" ^C12^ ^d^", "get_mem", 5, 0},
 	{" ^C3^  ^d^", "get_conn",10, 0},
+	{" ^C4^ ^d^",  "ovpn_status", 60, 0},
 	{" ^C13^ ^d^", "get_vol", 0, 10},
 	{" ^C1^ ^d^", "date '+%a %b %d, %Y  %H:%M'", 30, 0},
 };
